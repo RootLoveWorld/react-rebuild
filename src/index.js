94642123e76a69183a7f54c27826e7298a42b870
高伟 <gaowei232@crc.com.hk>
2025-09-28 17:56:46 +0800

    rebuild project

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TemperatureDemo from './components/TemperatureDemo';
-
-ReactDOM.render(
-    <TemperatureDemo/>
-    ,document.getElementById("app"));
